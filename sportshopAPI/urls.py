7f51ee64377d5f9c0a59bd376533786b65154c48
Christopher Browne <cbrowne@Christophers-MacBook-Pro.local>
2022-08-23 19:21:00 +0600

    Third Commit

--- sportshopAPI/urls.py
+++ sportshopAPI/urls.py
@@ -14,9 +14,36 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework.routers import SimpleRouter
+# from product.views import ProductViewSet
+# from django.conf.urls.static import static
+# from django.conf import settings
+# from cart.views import CartApiView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# router = SimpleRouter()
+# router.register('products', ProductViewSet)
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Sportshop_API",
+      default_version='v1',
+      description="test",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('api/v2/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     # path('api/v1/', include(router.urls)),
     path('api/v2/account/', include('account.urls')),
