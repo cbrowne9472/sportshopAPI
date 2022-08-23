d6ea3699db7952fa767731e018602c87284ce3ff
Raimbekvl <raimbekvl@gmail.com>
2022-08-23 19:45:11 +0600

    test3

--- sportshopAPI/settings.py
+++ sportshopAPI/settings.py
@@ -10,8 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-#Hello
-#hewqo
 
 from pathlib import Path
 from decouple import config
